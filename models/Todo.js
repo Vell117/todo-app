568359ea160e3bac93a9efd3477d4d49b9999d88
Vell117 <boss.vell@bk.ru>
2020-09-27 21:47:48 +0500

    New part of the frontend

---
+++ models/Todo.js
@@ -0,0 +1,19 @@
+// Подключаем пакет mongoose, и объект Schema
+const {Schema, model} = require('mongoose');
+
+// Передаем в конструктор объект конфигурации для модели
+const schema = new Schema({
+    // Пишем параметры для каждого todo
+    title: {
+        type: String, 
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        // Пишем по умолчанию false т.к, если мы только создали todo, то она ещё не завершина
+        default: false
+    }
+})
+
+// Экспортируем функцию model 
+module.exports = model('Todo', schema); 
--- routes/todos.js
+++ routes/todos.js
@@ -2,13 +2,34 @@
 // Подключаем из библиотеки Express
 const {Router} = require('express');
 
+// Подключаем написанную нами подель Todo 
+const Todo = require('../models/Todo');
+
 // Создаём переменную router из функции Router
 const router = Router();
 
 // Обращаемся к роутеру, чтобы обрабатывать get запросы
-router.get('/', (req, res) => {
+// Пишем асинхронно, т.к будем делать запрос к БД
+router.get('/', async (req, res) => {
+    // Получаем массив всех todos
+    const todos = await Todo.find({});
+
     // Ответ пользователю
-    res.render('index');
+    res.render('index', {
+        //Название вкладки
+        title: 'Todos list',
+        //Подсветка 
+        isIndex: true,
+        // Передаём массив todos как параметр для отображения на странице
+        todos
+    });
+})
+
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'Create todo',
+        isCreate: true
+    });
 })
 
 // экспортируем роутер из этого файла 
